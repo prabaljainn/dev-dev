d70e58c2ca83c914982560d85c280597f5185c57
prabaljainnnn <prabaljainnn@gmail.com>
2023-02-21 21:42:13 +0530

    boilerplate react setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `${counter} Times`;
+  }, [counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <button onClick={() => setCount(counter + 1)}>Click me </button>
+      <button>{counter}</button>
     </div>
   );
 }
